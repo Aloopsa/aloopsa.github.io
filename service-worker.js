3d1dc5f4cf7bd36124a250e75418a6ea17d2ec0a
Aloopsa <albert.lopez.ayora@gmail.com>
2026-09-19 13:03:46 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-nutricion-v1';
+const CACHE = 'mi-nutricion-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
